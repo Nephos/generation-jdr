89b014acf125fca7531016d6d37e74b7e8acd8ef
Arthur Poulet <arthur.poulet@mailoo.org>
2017-08-17 19:06:57 +0100

    Fix some coding style

--- lib/lieudit.php
+++ lib/lieudit.php
@@ -3,7 +3,7 @@
 global $db;
 
 function creerlieudit($comm) {
-  // pour éviter le lieu dit avec le nom dans la génération de village
+  // pour Ã©viter le lieu dit avec le nom dans la gÃ©nÃ©ration de village
   $ListeNom = array("Avygael", "Taklin");
   $lieudit='';
   $Lieu='';
@@ -11,34 +11,36 @@ function creerlieudit($comm) {
   $Qua='';
   $Nom='';
 
-  if ($comm == 0)
+  if ($comm == 0) {
     $trand = rand(1,100);
-  else
+  }
+  else {
     $trand = rand(1,95);
+  }
 
-  switch($trand) {
+  switch ($trand) {
     case $trand<25;
-    $Lieu=db_get_one_random_partial_value('LieuMas');
-    $Adj=db_get_one_random_partial_value('AdjMas');
-    $lieudit.=$Lieu.$Adj;
+    $Lieu = db_get_one_random_partial_value('LieuMas');
+    $Adj = db_get_one_random_partial_value('AdjMas');
+    $lieudit .= $Lieu.$Adj;
     break;
     case $trand>=25&&$trand<48;
-    $Lieu=db_get_one_random_partial_value('LieuFem');
-    $Adj=db_get_one_random_partial_value('AdjFem');
-    $lieudit.=$Lieu.$Adj;
+    $Lieu = db_get_one_random_partial_value('LieuFem');
+    $Adj = db_get_one_random_partial_value('AdjFem');
+    $lieudit .= $Lieu.$Adj;
     break;
     case $trand>=48&&$trand<76;
-    $Lieu=db_get_one_random_partial_value('QuaMas');
-    $Adj=db_get_one_random_partial_value('QuaMas');
-    $lieudit.=$Lieu.$Adj;
+    $Lieu = db_get_one_random_partial_value('QuaMas');
+    $Adj = db_get_one_random_partial_value('QuaMas');
+    $lieudit .= $Lieu.$Adj;
     break;
     case $trand>=76&&$trand<96;
-    $Lieu=db_get_one_random_partial_value('QuaFem');
-    $Adj=db_get_one_random_partial_value('QuaFem');
-    $lieudit.=$Lieu.$Adj;
+    $Lieu = db_get_one_random_partial_value('QuaFem');
+    $Adj = db_get_one_random_partial_value('QuaFem');
+    $lieudit .= $Lieu.$Adj;
     break;
     case ($trand>=96);
-    $lieudit.='Chez '.$ListeNom[rand(0,count($ListeNom)-1)];
+    $lieudit .= 'Chez '.$ListeNom[rand(0,count($ListeNom)-1)];
     break;
   }
   return $lieudit;
