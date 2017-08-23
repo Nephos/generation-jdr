2543d3709a599fc4500fd8b859052710d7322cbb
Arthur Poulet <arthur.poulet@mailoo.org>
2017-08-23 01:57:27 +0100

    Improve chateau

--- lib/chateau.php
+++ lib/chateau.php
@@ -2,14 +2,6 @@
 
 function creerchateau($taille)
 {
-    $taille1 = db_get_one_random_partial_value("ChateauTaille1");
-    $taille2 = db_get_one_random_partial_value("ChateauTaille2");
-    $taille3 = db_get_one_random_partial_value("ChateauTaille3");
-    $taille4 = db_get_one_random_partial_value("ChateauTaille4");
-    $taille5 = db_get_one_random_partial_value("ChateauTaille5");
-    $taille6 = db_get_one_random_partial_value("ChateauTaille6");
-    $taille7 = db_get_one_random_partial_value("ChateauTaille7");
-    $taille8 = db_get_one_random_partial_value("ChateauTaille8");
     $deterioration = array("");
     $amelioration = db_get_random_partial_values("ChateauAmelioration", 25);
     $chateau = db_get_one_random_partial_value("ChateauTaille$taille");
