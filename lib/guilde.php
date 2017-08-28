d5c3b9f0568a3f1412f8308c27d1305cf99054fe
Arthur Poulet <arthur.poulet@mailoo.org>
2017-08-28 21:10:38 +0200

    Guilde uses sql and move nom into another sql file

--- lib/guilde.php
+++ lib/guilde.php
@@ -3,8 +3,8 @@ function creerguilde()
 {
     include_once('nomevolue.php');
     $guilde='';
-    $typedeguilde=array("marchands","magiciens","sorciers","assassins","voleurs","roublards","commercants","troubadours");
+    $typedeguilde = db_get_one_random_partial_value("typedeguilde");
 
-    $guilde.='Guilde de '.$typedeguilde[rand(0, count($typedeguilde)-1)].' ayant pour chef '.creernomevo(rand(0, 8), rand(1, 2));
+    $guilde.='Guilde de '.$typedeguilde.' ayant pour chef '.creernomevo(rand(0, 8), rand(1, 2));
     return $guilde;
 }
