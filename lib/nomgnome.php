8eeece5b71b37070b6fcd008a4f94d340dd78b54
Arthur Poulet <arthur.poulet@mailoo.org>
2017-08-28 21:19:22 +0200

    Nom elf uses sql

--- lib/nomgnome.php
+++ lib/nomgnome.php
@@ -1,9 +1,9 @@
 <?php
 function getnomgnome()
 {
-    $table1=rand(1, 4);
-    $table2= array("add","arum","baer","bar","callad","chik","dal","din","eaus","enn","erf","faer","fen","flan","gaer","gar","hed","herl","ien","jan","kaer","len","lun","mikk","neb","oaen","ow","pall","pin","raer","ras","seg","skor","tikk","uran","urd","van","var","wann","wed");
-    $table3=array("Acier","Appeleur","Argen","Bière","Blaireau","Boucle","Brille","Buveur","Cape","Cendre","Coeur","Colline","Eclair","Enjambeur","Errant","Fantôme","Faux-pas","Fer","Gemme","Honneur","Leste","Lune","Main","Oeil","Ombre","Or","Os","Paresse","Peau","Péllant","Petit","Pied","Pierre","Rapide","Renard","Rieur","Ruse","Sauvage","Soleil","Soupir","Terre");
+    $table1 = rand(1, 4);
+    $table2 = array("add","arum","baer","bar","callad","chik","dal","din","eaus","enn","erf","faer","fen","flan","gaer","gar","hed","herl","ien","jan","kaer","len","lun","mikk","neb","oaen","ow","pall","pin","raer","ras","seg","skor","tikk","uran","urd","van","var","wann","wed");
+    $table3 = array("Acier","Appeleur","Argen","Bière","Blaireau","Boucle","Brille","Buveur","Cape","Cendre","Coeur","Colline","Eclair","Enjambeur","Errant","Fantôme","Faux-pas","Fer","Gemme","Honneur","Leste","Lune","Main","Oeil","Ombre","Or","Os","Paresse","Peau","Péllant","Petit","Pied","Pierre","Rapide","Renard","Rieur","Ruse","Sauvage","Soleil","Soupir","Terre");
     $nom='';
     switch ($table1) {
         case 1:
